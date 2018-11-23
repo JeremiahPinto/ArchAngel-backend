857cc01e9d72fc419f7492ea07fd7c7035504159
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2018-11-23 19:10:05 +0800

    Fixed test

--- test/services/auth_management.test.js
+++ test/services/auth_management.test.js
@@ -3,7 +3,7 @@ const app = require('../../src/app');
 
 describe('\'auth_management\' service', () => {
   it('registered the service', () => {
-    const service = app.service('auth-management');
+    const service = app.service('authManagement');
 
     assert.ok(service, 'Registered the service');
   });
